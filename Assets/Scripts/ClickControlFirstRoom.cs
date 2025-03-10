120088e0e69c3e338eb6eef9efdceb90e23477d1
gleemmy <gleemmy@tlu.ee>
2025-03-10 18:33:44 +0200

    Added rooms

---
+++ Assets/Scripts/ClickControlFirstRoom.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickControlFirstRoom : MonoBehaviour
+{
+    public GameObject MudStain;
+    public GameObject DryPlant;
+    public GameObject FreshPlant;
+
+    public Text NarrationTextFirstRoom;
+    
+
+    ptivate bool hasWaterCan = false;
+    private bool hasTowel = false;
+
+    private bool hasMudstain = false;  
+
+    private bool hasTowel
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
